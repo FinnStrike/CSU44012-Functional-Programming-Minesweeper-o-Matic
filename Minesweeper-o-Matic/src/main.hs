bb954f92cfc9d8f8e92dcc6bf2b9920f9d3e9911
FinnStrike <murphf15@tcd.ie>
2025-01-02 22:28:51 +0000

    Added Square Logic

--- Minesweeper-o-Matic/src/main.hs
+++ Minesweeper-o-Matic/src/main.hs
@@ -7,9 +7,6 @@ import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core hiding ((<|>))
 
 import Data.IORef
-import Text.Read (readMaybe)
-import Text.Parsec hiding (string)
-import Text.Parsec.String (Parser)
 
 -- User Interface
 
@@ -55,3 +52,44 @@ mkButtons = do
         #+ map element rows
 
     return [grid]
+
+data Block
+    = Mine
+    | Empty Int
+    deriving (Show, Eq)
+
+data State
+    = Hidden Block
+    | Revealed Block
+    deriving (Show, Eq)
+
+data Square
+    = Flagged State
+    | Clear State
+    deriving (Show, Eq)
+
+isMine :: Square -> Bool
+isMine (Flagged (Hidden Mine))   = True
+isMine (Flagged (Revealed Mine)) = True
+isMine (Clear (Hidden Mine))     = True
+isMine (Clear (Revealed Mine))   = True
+isMine _                         = False
+
+reveal :: Square -> Square
+reveal (Flagged (Hidden block)) = Clear (Revealed block)
+reveal (Clear (Hidden block))   = Clear (Revealed block)
+reveal square                   = square
+
+isRevealed :: Square -> Bool
+isRevealed (Flagged (Revealed _)) = True
+isRevealed (Clear (Revealed _))   = True
+isRevealed _                      = False
+
+flag :: Square -> Square
+flag (Flagged (Hidden block)) = Clear (Hidden block)
+flag (Clear (Hidden block))   = Flagged (Hidden block)
+flag square                   = square
+
+isFlagged :: Square -> Bool
+isFlagged (Flagged _) = True
+isFlagged _           = False
