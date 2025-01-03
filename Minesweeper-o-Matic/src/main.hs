d730de048b37b8ed9ffbf8e989dba590c3165f18
FinnStrike <murphf15@tcd.ie>
2025-01-03 18:13:53 +0000

    Integrated Square Logic with Buttons

--- Minesweeper-o-Matic/src/main.hs
+++ Minesweeper-o-Matic/src/main.hs
@@ -18,9 +18,12 @@ main = do
 setup :: Window -> UI ()
 setup w = void $ do
     return w # set title "Minesweeper-o-Matic"
-    UI.addStyleSheet w "grid.css"
-
-    buttons <- mkButtons
+    -- Prevent Default Context Menu from showing on Right Click
+    runFunction $ ffi "document.addEventListener('contextmenu', function(e) { e.preventDefault(); })"
+    -- Create Grid of Buttons
+    squaresRef <- liftIO $ newIORef createGrid
+    buttons <- mkButtons squaresRef
+    -- Display Grid
     getBody w #+ [UI.div #. "wrap" #+ (greet ++ map element buttons)]
 
 greet :: [UI Element]
@@ -28,46 +31,76 @@ greet =
     [ UI.h1  #+ [string "Minesweeper-o-Matic"]
     ]
 
-mkButton :: String -> UI (Element, Element)
-mkButton title = do
-    stateRef <- liftIO $ newIORef False
-    button <- UI.button #. "button" #+ [string title]
-                # set UI.style [("width", "25px"), ("height", "25px")]
+-- Create a new Button with corresponding Square
+mkButton :: IORef Square -> UI (Element, Element)
+mkButton squareRef = do
+    -- Create the Button Element and Style
+    button <- UI.button #. "button" # set UI.style [("width", "25px"), ("height", "25px")]
+    -- On Left Click we Reveal the Square
     on UI.click button $ \_ -> do
-        currentState <- liftIO $ readIORef stateRef
-        liftIO $ writeIORef stateRef (not currentState)
-        let newTitle = if currentState then "" else "X"
-        element button # set UI.text newTitle
+        currentSquare <- liftIO $ readIORef squareRef
+        let newSquare = case currentSquare of
+                Clear (Hidden cell)   -> Clear (Revealed cell)
+                _                     -> currentSquare
+        liftIO $ writeIORef squareRef newSquare
+        updateButton button newSquare 
+    -- On Right Click we Flag/Unflag the Square
+    on UI.contextmenu button $ \evt -> do
+        currentSquare <- liftIO $ readIORef squareRef
+        let newSquare = case currentSquare of
+                Clear (Hidden cell)   -> Flagged (Hidden cell)
+                Flagged (Hidden cell) -> Clear (Hidden cell)
+                _                     -> currentSquare
+        liftIO $ writeIORef squareRef newSquare
+        updateButton button newSquare
     view   <- UI.div #+ [element button]
     return (button, view)
 
-mkButtons :: UI [Element]
-mkButtons = do
-    rows <- forM [1..10] $ \_ -> do
-        rowButtons <- forM [1..10] $ \_ -> do
-            (b, v) <- mkButton ""
+-- Update the Button Text depending on State
+updateButton :: Element -> Square -> UI Element
+updateButton button square = do
+    let title = case square of
+            Flagged _                  -> "X"
+            Clear (Revealed Mine)      -> "*"
+            Clear (Revealed (Empty n)) -> show n
+            _                          -> ""
+    element button # set UI.text title
+
+-- Create the Grid of Buttons
+mkButtons :: IORef [[Square]] -> UI [Element]
+mkButtons squaresRef = do
+    -- Create 10 Columns
+    rows <- forM [0..9] $ \i -> do
+        -- Create 10 Rows
+        rowButtons <- forM [0..9] $ \j -> do
+            squares <- liftIO $ readIORef squaresRef
+            squareRef <- liftIO $ newIORef (squares !! i !! j)
+            (b, v) <- mkButton squareRef
             return $ element v
         UI.div # set UI.style [("display", "inline-flex")] #+ rowButtons
     grid <- UI.div # set UI.style [("display", "flex"), ("flex-direction", "column")]
         #+ map element rows
-
     return [grid]
 
-data Block
+-- Base Square Type
+data Cell
     = Mine
     | Empty Int
     deriving (Show, Eq)
 
+-- State Square Type
 data State
-    = Hidden Block
-    | Revealed Block
+    = Hidden Cell
+    | Revealed Cell
     deriving (Show, Eq)
 
+-- Full Square Type
 data Square
     = Flagged State
     | Clear State
     deriving (Show, Eq)
 
+-- Check if a Square is a Mine
 isMine :: Square -> Bool
 isMine (Flagged (Hidden Mine))   = True
 isMine (Flagged (Revealed Mine)) = True
@@ -75,21 +108,29 @@ isMine (Clear (Hidden Mine))     = True
 isMine (Clear (Revealed Mine))   = True
 isMine _                         = False
 
+-- Reveal a Square
 reveal :: Square -> Square
-reveal (Flagged (Hidden block)) = Clear (Revealed block)
-reveal (Clear (Hidden block))   = Clear (Revealed block)
+reveal (Flagged (Hidden cell)) = Clear (Revealed cell)
+reveal (Clear (Hidden cell))   = Clear (Revealed cell)
 reveal square                   = square
 
+-- Check if a Square is Revealed
 isRevealed :: Square -> Bool
 isRevealed (Flagged (Revealed _)) = True
 isRevealed (Clear (Revealed _))   = True
 isRevealed _                      = False
 
+-- Flag a Square
 flag :: Square -> Square
-flag (Flagged (Hidden block)) = Clear (Hidden block)
-flag (Clear (Hidden block))   = Flagged (Hidden block)
+flag (Flagged (Hidden cell)) = Clear (Hidden cell)
+flag (Clear (Hidden cell))   = Flagged (Hidden cell)
 flag square                   = square
 
+-- Check if a Square is Flagged
 isFlagged :: Square -> Bool
 isFlagged (Flagged _) = True
 isFlagged _           = False
+
+-- Create a 10x10 Grid of Squares
+createGrid :: [[Square]]
+createGrid = replicate 10 (replicate 10 (Clear (Hidden (Empty 0))))
